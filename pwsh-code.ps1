2b7544f8065e7dcd8e68f915f3c49105a12dd6f0
unknown <rcharithapallom@gmail.com>
2023-09-11 08:04:23 -0400

    Initial commit

---
+++ pwsh-code.ps1
@@ -0,0 +1,41 @@
+# Define your GitHub personal access token
+$githubToken = "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"
+
+# Define the repository name
+$repoName = "Tibco-aws-repo"
+
+# Define the GitHub username or organization where you want to create the repository
+$githubUsernameOrOrg = "Shinchan-24"
+
+$repodir = "C:\TIBCO\git-repo"
+
+# Create the new repository on GitHub using the GitHub API with the personal access token
+$uri = "https://api.github.com/user/repos"
+$headers = @{
+    "Authorization" = "token $githubToken"
+}
+$body = @{
+    name = $repoName
+}
+$response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -ContentType "application/json" -Body ($body | ConvertTo-Json)
+
+# Extract the Git repository URL from the response
+$repoUrl = $response.html_url
+
+# Print the repository URL
+Write-Host "Repository created: $repoUrl"
+
+# Initialize a Git repository locally
+git init
+
+copy-item -Path "C:\BMO\skeleton" -Destination "C:\TIBCO\git-repo" -PassThru
+
+# Add and commit some files
+git add .
+git commit -m "Initial commit"
+
+# Set the remote origin to the GitHub repository
+git remote add origin $repoUrl
+
+# Push the committed changes to GitHub
+git push -u origin master
